7d55d4f827f442357ed2389e51f0efd7683e2e9b
camillegiuliano <camille.giuliano@nrcan-rncan.gc.ca>
2025-03-03 11:35:16 -0800

    testing GHA with renv

--- RUNME.R
+++ RUNME.R
@@ -1,33 +1,28 @@
 
 ## this manual must be knitted by running this script
 
-prjDir <- SpaDES.project::findProjectPath()
+prjDir <- SpaDES.project::findProjectPath() #this might not be needed. 
 
-Require::Require(c("yaml", "purrr", "fs"))
+# Require::Require(c("yaml", "purrr", "fs"))
 
 docsDir <- file.path(prjDir, "_bookdown.yml") |>
   yaml::read_yaml() |>
   purrr::pluck("output_dir") |>
   fs::path_abs()
 
-pkgPath <- normalizePath(file.path("packages", version$platform,
-                                   paste0(version$major, ".", strsplit(version$minor, "[.]")[[1]][1])),
-                         winslash = "/")
-dir.create(pkgPath, recursive = TRUE)
-.libPaths(pkgPath, include.site = FALSE)
+# pkgPath <- normalizePath(file.path("packages", version$platform,
+                         #           paste0(version$major, ".", strsplit(version$minor, "[.]")[[1]][1])),
+                         # winslash = "/")
+# dir.create(pkgPath, recursive = TRUE)
+# .libPaths(pkgPath, include.site = FALSE)
 
-Require::Require(c("bookdown", "ROpenSci/bibtex", "data.table", "downlit",
-                   "formatR", "git2r", "kableExtra", "yihui/knitr", 
-                   "fansi", "xml2", "vctrs", "RefManageR", "remotes", "git2r"))
-remotes::install_github("PredictiveEcology/SpaDES.docs@development")
-
+# Require::Require(c("bookdown", "ROpenSci/bibtex", "data.table", "downlit",
+                   # "formatR", "git2r", "kableExtra", "yihui/knitr", 
+                   # "fansi", "xml2", "vctrs", "RefManageR", "remotes", "git2r"))
+# remotes::install_github("PredictiveEcology/SpaDES.docs@development")
 
 # bibDir <- Require::checkPath(file.path(prjDir, "citations"), create = TRUE)
 # figDir <- Require::checkPath(file.path(docsDir, "figures"), create = TRUE)
-
-options(
-  Ncpus = min(parallel::detectCores() / 2, 8)
-)
 
 # load packages -------------------------------------
 
@@ -44,7 +39,7 @@ library(formatR)
 ## automatically create a bib database for R packages
 # allPkgs <- .packages(all.available = TRUE, lib.loc = .libPaths()[1])
 # keyPkgs <- c(
-#   "bookdown", "knitr", "LandR", "LandWebUtils",
+#   "bookdown", "knitr", "LandR", "CBMutils,
 #   "reproducible", "rmarkdown", "shiny", "SpaDES.core", "SpaDES.tools"
 # )
 # write_bib(allPkgs, file.path(bibDir, "packages.bib")) ## TODO: using allPkgs, not all pkgs have dates/years
